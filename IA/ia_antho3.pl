49f9af234d0c3c93af5a7be61511f3a1009c182d
Anthony Faraut <anthony.faraut@gmail.com>
2014-10-23 12:06:57 +0200

    IA3's Explanation

--- IA/ia_antho3.pl
+++ IA/ia_antho3.pl
@@ -1,6 +1,16 @@
 :- dynamic shots/4.
 shots(JOUEURTEST, 0,0,0).
 
+%  X _ _ X _ _ X _ _ X
+%  _ X _ _ X _ _ X _ _
+%  _ _ X _ _ X _ _ X _
+%  X _ _ X _ _ X _ _ X
+%  _ X _ _ X _ _ X _ _
+%  _ _ X _ _ X _ _ X _
+%  X _ _ X _ _ X _ _ X
+%  _ X _ _ X _ _ X _ _
+%  _ _ X _ _ X _ _ X _
+
 /*34 Length of the list */
 gridlines(joueur1, 34, [coord(1, 1), coord(4, 1), coord(7, 1), coord(10, 1), coord(2, 2), coord(5, 2), coord(8, 2), coord(3, 3), coord(6, 3), coord(9, 3), coord(1, 4), coord(4, 4), coord(7, 4), coord(10, 4), coord(2, 5), coord(5, 5), coord(8, 5), coord(3, 6), coord(6, 6), coord(9, 6), coord(1, 7), coord(4, 7), coord(7, 7), coord(10, 7), coord(2, 8), coord(5, 8), coord(8, 8), coord(3, 9), coord(6, 9), coord(9, 9), coord(1, 10), coord(4, 10), coord(7, 10), coord(10, 10)]).
 
